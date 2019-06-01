960106212b65f0fd2ad513fd5b6bd0ade4c8db92
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-01 13:48:11 -0500

    rsvps buttons are working

--- src/javascripts/helpers/smash.js
+++ src/javascripts/helpers/smash.js
@@ -3,7 +3,7 @@ const friendRsvps = (friends, rsvps) => friends.map((friend) => {
   const f = friend;
   const rsvp = rsvps.find(r => r.friendId === f.id);
   if (rsvp) {
-    f.rsvpId = rsvp.Id;
+    f.rsvpId = rsvp.id;
     f.statusId = rsvp.statusId;
   }
 
